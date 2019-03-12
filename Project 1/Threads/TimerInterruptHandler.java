6ee3350848836870c1b951f07b0f73b715f4f3ac
Daniel Garcia <danielg_97@yahoo.com>
2019-03-12 01:52:18 -0400

    Added handleInterrupt functionality

--- Project 1/Threads/TimerInterruptHandler.java
+++ Project 1/Threads/TimerInterruptHandler.java
@@ -16,33 +16,28 @@ import osp.Hardware.*;
  * projects.
 */
 
-/**    
-       The timer interrupt handler.  This class is called upon to
-       handle timer interrupts.
-
-       @OSPProject Threads
-*/
-public class TimerInterruptHandler extends IflTimerInterruptHandler
-{
+/**
+ * The timer interrupt handler. This class is called upon to handle timer
+ * interrupts.
+ * 
+ * @OSPProject Threads
+ */
+public class TimerInterruptHandler extends IflTimerInterruptHandler {
     /**
-       This basically only needs to reset the times and dispatch
-       another process.
-
-       @OSPProject Threads
-    */
-    public void do_handleInterrupt()
-    {
-        // your code goes here
-
+     * This basically only needs to reset the times and dispatch another process.
+     * 
+     * @OSPProject Threads
+     */
+    public void do_handleInterrupt() {
+        IflThreadCB.dispatch();
     }
 
-
     /*
-       Feel free to add methods/fields to improve the readability of your code
-    */
+     * Feel free to add methods/fields to improve the readability of your code
+     */
 
 }
 
 /*
-      Feel free to add local classes to improve the readability of your code
-*/
+ * Feel free to add local classes to improve the readability of your code
+ */
